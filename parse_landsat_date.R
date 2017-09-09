edabd587e5c015f55a711803ad19ceccfd2fb445
Dhemerson Conciani <dhemerson_conciani@yahoo.com.br>
2017-09-09 19:11:46 -0300

    Update and rename parse_ladsat_imgdate.R to parse_landsat_date.R

--- parse_landsat_date.R
+++ parse_landsat_date.R
@@ -1,20 +1,25 @@
+# Retrieve date from landsat .tif filenames and use as string 
+# INPUT:  Path with raster files
+# OUTPUT: Object string
+
+# Read packages
 library(stringr)
 library(lubridate)
 library(tools)
 
-#get the list of rasters
+# Get list of raster files
 images <- list.files(path = 'H:/~~/',
                   pattern = '.tif$',
                   full.names = T)
 images.count <- length(nbr)
 
-#get names
+# Get names without extension
 list_names <- file_path_sans_ext(basename(images))
 
-#extract the third separator (date in Landsat 5, 7 and 8 filenames)
+# To USGS products (TM, ETM+ and OLI) the third separator is the aquisiton date of image, extract this:
 parse= sapply(strsplit(list_names, split='_', fixed=TRUE), function(x) (x[3]))
 
-#convert to date
+# Convert to date string
 date= ymd(parse)
 
 date
